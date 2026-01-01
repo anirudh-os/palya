fefef264a8e639693ee8835b305553efec27efdb
Anirudh Sripada Koundinya <anirudhsripada05@gmail.com>
2026-01-01 19:11:55 +0530

    refactor(core): migrate to minijinja and optimize render pipeline
    - Replace Tera with Minijinja for improved performance
    - Implement manual template loading via `load_templates` utility
    - Decouple `Post` internal state from `PostContext` serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use anyhow::{Context, Result};
+use minijinja::Environment;
 use pulldown_cmark::{Parser, html};
 use serde::{Deserialize, Serialize};
 use serde_yaml_ng::from_str;
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+use walkdir::WalkDir;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FrontMatter {
@@ -12,11 +17,17 @@ pub struct FrontMatter {
     pub template: Option<String>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Post {
     pub frontmatter: Option<FrontMatter>,
     pub body: String,
-    pub output_path: PathBuf,
+}
+
+#[derive(Serialize)]
+pub struct PostContext<'a> {
+    pub title: Option<&'a str>,
+    pub date: Option<&'a str>,
+    pub content: &'a str,
 }
 
 impl Post {
@@ -42,18 +53,9 @@ impl Post {
         let mut html_output = String::new();
         html::push_html(&mut html_output, parser);
 
-        let slug = frontmatter
-            .as_ref()
-            .and_then(|fm| fm.slug.as_deref())
-            .or_else(|| input_path.file_stem().and_then(|s| s.to_str()))
-            .unwrap_or("post1");
-
-        let output_path = PathBuf::from("dist").join(slug).join("index.html");
-
         Ok(Post {
             frontmatter,
             body: html_output,
-            output_path: PathBuf::from(output_path),
         })
     }
 
@@ -63,4 +65,39 @@ impl Post {
             .and_then(|fm| fm.template.as_deref())
             .unwrap_or("post.html")
     }
+
+    pub fn output_path(&self, input_path: PathBuf) -> PathBuf {
+        let slug = self
+            .frontmatter
+            .as_ref()
+            .and_then(|fm| fm.slug.as_deref())
+            .or_else(|| input_path.file_stem().and_then(|s| s.to_str()))
+            .unwrap_or("post1");
+
+        PathBuf::from("dist").join(slug).join("index.html")
+    }
+
+    pub fn as_context(&self) -> PostContext<'_> {
+        PostContext {
+            title: self.frontmatter.as_ref().and_then(|f| f.title.as_deref()),
+            date: self.frontmatter.as_ref().and_then(|f| f.date.as_deref()),
+            content: &self.body,
+        }
+    }
+}
+
+pub fn load_templates(env: &mut Environment, dir: &Path) -> Result<()> {
+    for entry in WalkDir::new(dir) {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let name = path.file_name().unwrap().to_string_lossy().into_owned(); 
+
+            let content = fs::read_to_string(path)?;
+
+            env.add_template_owned(name, content)?;
+        }
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use anyhow::Result;
-use my_ssg::Post;
-use std::{fs::File, io::Write};
-use tera::{Context, Tera};
+use anyhow::{Context, Result};
+use minijinja::{Environment};
+use my_ssg::{Post, load_templates};
+use std::{
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
 use walkdir::WalkDir;
 
 fn main() -> Result<()> {
-    let mut posts: Vec<Post> = Vec::new();
+    let mut posts: Vec<(Post, PathBuf)> = Vec::new();
 
     for entry in WalkDir::new("test_bench/content") {
         let entry = match entry {
@@ -23,31 +27,33 @@ fn main() -> Result<()> {
         }
 
         match Post::from_file(path.to_path_buf()) {
-            Ok(post) => posts.push(post),
+            Ok(post) => posts.push((post.clone(), post.output_path(path.to_path_buf()))),
             Err(err) => {
                 eprintln!("Failed to parse {:?}: {err}", path);
                 continue;
             }
-        }
+        };
     }
 
-    let tera = Tera::new("test_bench/templates/**/*.html")?;
-    for post in posts {
-        let mut context = Context::new();
-        if let Some(fm) = post.frontmatter.as_ref() {
-            context.insert("title", &fm.title);
-            context.insert("date", &fm.date);
+    let mut env = Environment::new();
+    load_templates(&mut env, Path::new("test_bench/templates"))?;
+
+    for (post, output_path) in &posts {
+        let template_name = post.template_name();
+
+        let output = env
+            .get_template(template_name)
+            .with_context(|| format!("Template {} is not found!", template_name))?
+            .render(post.as_context())
+            .with_context(|| format!("Couldn't render the template {}!", template_name))?;
+
+        if let Some(parent) = output_path.parent() {
+            std::fs::create_dir_all(parent).context("Couldn't create the dorectory")?;
         }
-        context.insert("content", &post.body);
 
-        let output = tera.render(post.template_name(), &context)?;
-
-        if let Some(parent) = post.output_path.parent() {
-            std::fs::create_dir_all(parent)?;
-        }
-
-        let mut file = File::create(&post.output_path)?;
-        file.write_all(output.as_bytes())?;
+        let mut file = File::create(output_path).context("Couldn't create the output file!")?;
+        file.write_all(output.as_bytes())
+            .context("Couldn't write to the output file!")?;
     }
 
     Ok(())
